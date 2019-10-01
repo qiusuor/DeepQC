8d22c1a5e1f334531df3ed1cfb9402ad2ce4ee24
qiusuor <750435412@qq.com>
2019-10-01 15:25:56 +0800

    Update train.sh

--- python/train.sh
+++ python/train.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x -e 
+
 rm data/* -rf
 
 ln -s $1 data/input.fastq
@@ -8,10 +10,8 @@ python extract_qual.py data/input.fastq data/input.qs
 
 python select_train_eval.py data/input.qs
 
-python train.py --epoch 1000 --val_batch_size 10240 --train_batch_size 10240
+python train.py --epoch 1000 --val_batch_size 10240 --train_batch_size 1
 
 python dumpModel.py
 
 python get_stat_info.py data/test.qs
-
-
