a6798a2d259755595067a1cdd430c313996b5529
qiusuor <750435412@qq.com>
2019-10-03 16:42:16 +0800

    fix

--- example/run.sh
+++ example/run.sh
@@ -1,6 +1,6 @@
-../c++/DeepQC c ../pre_trained_models/Hiseq2000_1.stat_info ../pre_trained_models/Hiseq2000_1.pt test.qs test.bin 20
+../c++/DeepQC c ../pre_trained_models/Hiseq2000_len_101_1.stat_info ../pre_trained_models/Hiseq2000_len_101_1.pt test.qs test.bin 20
 
-../c++/DeepQC x ../pre_trained_models/Hiseq2000_1.stat_info ../pre_trained_models/Hiseq2000_1.pt test.bin test.rec.qs 20
+../c++/DeepQC x ../pre_trained_models/Hiseq2000_len_101_1.stat_info ../pre_trained_models/Hiseq2000_len_101_1.pt test.bin test.rec.qs 20
 
 md5sum test.qs test.rec.qs
 
