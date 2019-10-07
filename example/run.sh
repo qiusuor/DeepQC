2c7dc8e7d1bbffba2157d9974b6ed899f5116c33
qiusuor <750435412@qq.com>
2019-10-07 17:53:39 +0800

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,8 +1,9 @@
+set -x -e
 ../c++/DeepQC --usage
 
 ../c++/DeepQC c ../pre_trained_models/Hiseq2000_len_101_1.stat_info ../pre_trained_models/Hiseq2000_len_101_1.pt test.qs test.bin 20 2>/dev/null
 
-../c++/DeepQC x ../pre_trained_models/Hiseq2000_len_101_1.stat_info ../pre_trained_models/Hiseq2000_len_101_1.pt test.bin test.rec.qs 20 2/dev/null
+../c++/DeepQC x ../pre_trained_models/Hiseq2000_len_101_1.stat_info ../pre_trained_models/Hiseq2000_len_101_1.pt test.bin test.rec.qs 20 2>/dev/null
 
 md5sum test.qs test.rec.qs
 
